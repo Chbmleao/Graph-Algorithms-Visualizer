406a654bdbf0fecbc16f2a4b3fe01b6554c3a46e
Carlos Leão <caikleao@gmail.com>
2023-07-10 17:06:42 -0300

    Add graph to main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Graph from "./components/Graph";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Graph />
     </div>
   );
 }
