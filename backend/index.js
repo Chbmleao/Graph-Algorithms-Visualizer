e7c4625c4c75952bf20482675fa4b4601dfe9818
Carlos Leão <caikleao@gmail.com>
2023-07-12 15:42:54 -0300

    Add post addWall route and remove post neighbor route

--- backend/index.js
+++ backend/index.js
@@ -8,31 +8,24 @@ const graph = new Graph(numTableRows, numTableCols);
 
 graph.printGraph();
 
-graph.addWall(5);
-
-graph.printGraph();
-
 const app = express();
+app.use(express.json());
 app.use(cors());
 const PORT = 5000;
 
 app.post("/api/addWall", (req, res) => {
-  const { cellIndex } = req.body;
-  console.log(cellIndex);
+  const wallCoordinates = req.body;
+  console.log(wallCoordinates);
+
+  graph.addWall(wallCoordinates);
+
+  res.status(200).json({ message: "Add wall sucessfully" });
 });
 
 app.get("/api/tableSize", (req, res) => {
   const tableSize = graph.getTableSize();
 
   res.json(tableSize);
-});
-
-app.post("/api/addNeighbors", (req, res) => {
-  const data = req.body;
-
-  res.status(200).json({
-    message: "Add neighborhood successfully",
-  });
 });
 
 app.listen(PORT, () => {
