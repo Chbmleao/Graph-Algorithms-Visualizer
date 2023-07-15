01e6b2ae8b49bea78043c24b92169d61335900d7
Carlos Leão <caikleao@gmail.com>
2023-07-15 11:50:17 -0300

    Add drag and drop provider to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 import Navbar from "./components/Navbar";
 import Table from "./components/Table";
@@ -27,10 +29,12 @@ function App() {
   };
 
   return (
-    <div>
-      <Navbar onClearClick={handleClearClick} />
-      <Table cellColors={cellColors} setCellColors={setCellColors} />
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div>
+        <Navbar onClearClick={handleClearClick} />
+        <Table cellColors={cellColors} setCellColors={setCellColors} />
+      </div>
+    </DndProvider>
   );
 }
 
