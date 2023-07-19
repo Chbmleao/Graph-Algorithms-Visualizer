10c0d7fccd80d9ab1edb580db523c64d645127b2
Carlos Leão <caikleao@gmail.com>
2023-07-19 12:14:05 -0300

    Removed unnecessary console.log and restructure algorithm paths

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const Graph = require("./Graph");
 
-const numTableRows = 20;
-const numTableCols = 30;
+const numTableRows = 4;
+const numTableCols = 4;
 const graph = new Graph(numTableRows, numTableCols);
-
-graph.printGraph();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-const PORT = 5000;
 
 app.post("/api/addWall", (req, res) => {
   const wallCoordinates = req.body;
 
   graph.addWall(wallCoordinates);
-
-  graph.printGraph();
 
   res.status(200).json({ message: "Wall added successfully" });
 });
@@ -28,15 +23,11 @@ app.delete("/api/removeWall", (req, res) => {
 
   graph.removeWall(wallCoordinates);
 
-  graph.printGraph();
-
   res.status(200).json({ message: "Wall removed successfully" });
 });
 
 app.delete("/api/removeAllWalls", (req, res) => {
   graph.removeAllWalls();
-
-  graph.printGraph();
 
   res.status(200).json({ message: "All walls removed successfully" });
 });
@@ -50,21 +41,35 @@ app.get("/api/tableSize", (req, res) => {
 app.post("/api/algorithm/dijkstra", (req, res) => {
   const { startCoordinates, endCoordinates } = req.body.coordinates;
 
+  const startIndex = graph.getVerticeIndex(startCoordinates);
+  const endIndex = graph.getVerticeIndex(endCoordinates);
+
+  const dists = graph.dijkstra(startIndex);
+
   res.status(200).json({ message: "Dijkstra algorithm executed" });
 });
 
 app.post("/api/algorithm/bfs", (req, res) => {
   const { startCoordinates, endCoordinates } = req.body.coordinates;
 
-  res.status(200).json({ message: "BFS algorithm executed" });
+  const startIndex = graph.getVerticeIndex(startCoordinates);
+  const endIndex = graph.getVerticeIndex(endCoordinates);
+
+  const path = graph.bfs(startIndex, endIndex);
+
+  res.status(200).json({ message: "BFS algorithm executed", path: path });
 });
 
 app.post("/api/algorithm/dfs", (req, res) => {
   const { startCoordinates, endCoordinates } = req.body.coordinates;
 
+  const startIndex = graph.getVerticeIndex(startCoordinates);
+  const endIndex = graph.getVerticeIndex(endCoordinates);
+
   res.status(200).json({ message: "DFS algorithm executed" });
 });
 
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
