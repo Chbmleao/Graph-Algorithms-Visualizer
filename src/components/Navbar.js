84d129e1d8af31af9aeae7dc4d1c1c49a5624f07
Carlos Leão <caikleao@gmail.com>
2023-07-20 15:33:03 -0300

    Removed unnecessary imports

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
 import "../styles/NavbarStyles.css";
 
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 import SelectBox from "./SelectBox";
 
