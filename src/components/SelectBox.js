8a3b5621e63b0eed6bb8809053d6c4cfecfff31e
Carlos Leão <caikleao@gmail.com>
2023-08-01 09:41:11 -0300

    Add dijkstra algorithm as default

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -12,9 +12,6 @@ const SelectBox = ({ onAlgorithmSelect }) => {
   return (
     <div className="select">
       <select name="algorithms" onChange={handleAlgorithmChange}>
-        <option defaultValue value="none">
-          Select an algorithm
-        </option>
         <option value="dijkstra">Dijkstra</option>
         <option value="bfs">Breadth-first search (BFS)</option>
         <option value="dfs">Depth-first search (DFS)</option>
